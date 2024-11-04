be68bc98945e898a800ff10304ddd784fea133a5
sonw young woo <sonwoo@ecis.co.kr>
2024-11-04 16:49:02 +0900

    Resolve merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:ship_5bv_app/screen/auth_screen.dart';
 void main() {
   runApp(
     //시작 테스트2
-    //시작
+    //시작 테스트1
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: AuthScreen(),
