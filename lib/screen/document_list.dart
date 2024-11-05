0f4abc54a29599e380d97fe25814439be8d658ae
sonw young woo <sonwoo@ecis.co.kr>
2024-11-05 17:26:54 +0900

    Resolve merge conflicts

---
+++ lib/screen/document_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class DocumentList extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+        title: const Text('신고서 목록 화면'),
+        centerTitle: true,
+        )
+    );
+  }
+
+}
