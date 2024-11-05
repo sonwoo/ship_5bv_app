e3b873d1adaaf8fc098e2e67e8427dd773c6de16
Yang <sksungtj@ecis.co.kr>
2024-11-05 09:22:59 +0900

    Resolve merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:ship_5bv_app/screen/auth_screen.dart';
 
 void main() {
   runApp(
-    //시작 테스트3
+    //시작 테스트7
     //시작 테스트1
     MaterialApp(
       debugShowCheckedModeBanner: false,
