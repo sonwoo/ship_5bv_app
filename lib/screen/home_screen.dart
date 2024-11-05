cdb15a4910af00205a996fe9e28ff8aace56b8b9
sonw young woo <sonwoo@ecis.co.kr>
2024-11-05 17:28:36 +0900

    Resolve merge conflicts

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ship_5bv_app/screen/auth_screen.dart';
+import 'package:ship_5bv_app/screen/document_list.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -10,23 +12,33 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen>{
 
+  final List<Widget> _tabs = [
+    DocumentList(),
+    DocumentList(),
+    DocumentList()
+  ];
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: const Text('선용품 이행 완료 보고'),
-        centerTitle: true,
-        bottom: const TabBar(
-           tabs: [
-             Tab(icon: Icon(Icons.home), text: '적재'),
-             Tab(icon: Icon(Icons.favorite), text: '하선'),
-             Tab(icon: Icon(Icons.person), text: '환적'),
-           ],
+          appBar: AppBar(
+          backgroundColor: Colors.amber,
+          title: const Text('선용품 이행 완료 보고'),
+          centerTitle: true,
+          bottom: const TabBar(
+             tabs: [
+               Tab(icon: Icon(Icons.home), text: '적재'),
+               Tab(icon: Icon(Icons.favorite), text: '하선'),
+               Tab(icon: Icon(Icons.person), text: '환적'),
+             ],
+          ),
         ),
-      ),
+        body: TabBarView(
+          physics: NeverScrollableScrollPhysics(),
+          children: _tabs,
+        ),
       )
     );
 
