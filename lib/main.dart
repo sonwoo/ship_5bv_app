87f31e7572c156ab5a40e6a791bfeeb1ade9d599
sonw young woo <sonwoo@ecis.co.kr>
2024-11-06 13:32:57 +0900

    Resolve merge conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:ship_5bv_app/screen/auth_screen.dart';
+import 'package:ship_5bv_app/screen/home_screen.dart';
 
 void main() {
   runApp(
@@ -7,7 +9,12 @@ void main() {
     //시작 테스트1
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthScreen(),
+      localizationsDelegates:  [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      home: HomeScreen(),
     )
   );
 }
--- lib/screen/document_list.dart
+++ lib/screen/document_list.dart
@@ -1,15 +1,86 @@
 import 'package:flutter/material.dart';
 
 
-class DocumentList extends StatelessWidget{
+class DocumentList extends StatefulWidget{
+
+  const DocumentList({super.key});
+
+  @override
+  State<StatefulWidget> createState()  => _DocumentList();
+
+}
+
+class _DocumentList extends State<DocumentList>{
+
+  DateTime start_date = DateTime.now().subtract(const Duration(days: 6));
+  DateTime end_date = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text('신고서 목록 화면'),
         centerTitle: true,
-        )
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(padding: EdgeInsets.all(15)),
+                Container(
+                  height: 30,
+                  width: 60,
+                  child: Text('신고일자'),
+                ),
+                SizedBox( height: 30, width: 30,),
+                ElevatedButton(
+                  onPressed: () async {
+                    final selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: start_date,
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime.now(),
+                      initialEntryMode: DatePickerEntryMode.calendarOnly,
+                      locale: const Locale('ko','KR'),
+                    );
+                    if(selectedDate != null){
+                      setState(() {
+                        start_date = selectedDate;
+                      });
+                    }
+                  },
+                  child: Text("${start_date.year.toString()}-${start_date.month.toString().padLeft(2, '0')}-${start_date.day.toString().padLeft(2, '0')}"),
+                ),
+                Icon(Icons.accessibility_new),
+                ElevatedButton(
+                  onPressed: () async {
+                    final selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: end_date,
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime.now(),
+                      initialEntryMode: DatePickerEntryMode.calendarOnly,
+                      locale: const Locale('ko','KR'),
+                    );
+                    if(selectedDate != null){
+                      setState(() {
+                        end_date = selectedDate;
+                      });
+                    }
+                  },
+                  child: Text("${end_date.year.toString()}-${end_date.month.toString().padLeft(2, '0')}-${end_date.day.toString().padLeft(2, '0')}"),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 
