88d52a42be89841ad2a1d247e9f4bc0d912092cb
Yang <sksungtj@ecis.co.kr>
2024-11-07 17:12:35 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/screen/anchorage_screen.dart
+++ lib/screen/anchorage_screen.dart
@@ -1,47 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class EmployeeCard extends StatelessWidget {
-  final Employee employee;
+class AnchorageCard extends StatelessWidget {
+  final Anchorage anchora;
 
-  const EmployeeCard({Key? key, required this.employee}) : super(key: key);
+  const AnchorageCard({Key? key, required this.anchora}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  return Card(
-  child: ListTile(
-  leading: CircleAvatar(
-  backgroundImage: NetworkImage(
-  employee.imageUrl),
-  ),
-  title: Text(employee.name),
-  subtitle: Text(employee.department),
-  onTap: () {
-  // 상세 정보 화면으로 이동
-  Navigator.push(
-  context,
-  MaterialPageRoute(
-  builder: (BuildContext context){
-
-    return Container(
-      height: 200,
-      child: Center(
-        child: Text('내용'),
+    return Card(
+      child: ListTile(
+        leading: CircleAvatar(
+          //backgroundImage: NetworkImage(Anchorage.imageUrl),
+        ),
+        title: Text('a'),
+        subtitle: Text('a'),
+        onTap: () {
+          // 상세 정보 화면으로 이동
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (BuildContext context) {
+                return Container(
+                  height: 200,
+                  child: Center(
+                    child: Text('내용'),
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
-  },
-  ),
-  );
-  },
-  ),
-  );
   }
 }
 
-class Employee {
+class Anchorage {
   final String name;
-  final String department;
-  final String imageUrl;
-
-  Employee({required this.name, required this.department, required this.imageUrl});
+  Anchorage(
+      {required this.name});
 }
