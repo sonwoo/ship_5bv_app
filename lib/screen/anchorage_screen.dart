9d751bc058ae26a1ede6546a4e6da08ea726c4f3
Yang <sksungtj@ecis.co.kr>
2024-11-08 10:11:29 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/screen/anchorage_screen.dart
+++ lib/screen/anchorage_screen.dart
@@ -1,43 +1,91 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-class AnchorageCard extends StatelessWidget {
-  final Anchorage anchora;
-
-  const AnchorageCard({Key? key, required this.anchora}) : super(key: key);
-
+class Anchorage_screen extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: CircleAvatar(
-          //backgroundImage: NetworkImage(Anchorage.imageUrl),
-        ),
-        title: Text('a'),
-        subtitle: Text('a'),
-        onTap: () {
-          // 상세 정보 화면으로 이동
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (BuildContext context) {
-                return Container(
-                  height: 200,
-                  child: Center(
-                    child: Text('내용'),
-                  ),
-                );
-              },
-            ),
-          );
-        },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: Anchorage(),
     );
   }
 }
 
-class Anchorage {
-  final String name;
-  Anchorage(
-      {required this.name});
+class Anchorage extends StatefulWidget {
+  @override
+  _AnchorageState createState() => _AnchorageState();
+}
+
+class _AnchorageState extends State<Anchorage> {
+  final List<String> comments = <String>[
+    '최신 음악 무제한',
+    '뮤직비디오 무제한',
+    '음악 라이브러리 공유',
+    '뮤직비디오 업로드',
+    '뮤직비디오 공유 무제한'
+  ];
+
+  final List<IconData> icons = <IconData>[
+    Icons.music_note,
+    Icons.music_video,
+    Icons.library_music,
+    Icons.videocam,
+    Icons.video_library
+  ];
+
+  final List<String> wons = <String>[
+    "5,000",
+    "7,000",
+    "9,000",
+    "10,000",
+    "15,000",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MINOKUMA - FLUTTER'),
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.all(8),
+        itemCount: comments.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(
+                    icons[index],
+                    color: Colors.redAccent,
+                  ),
+                  title: Text('${comments[index]}'),
+                  subtitle: Text(
+                    '가격 : ${wons[index]} 원',
+                  ),
+                ),
+                ButtonBar(
+                  children: <Widget>[
+                    TextButton(
+                      child: Text('확인'),
+                      onPressed: () {},
+                      style: TextButton.styleFrom(backgroundColor: Colors.purple),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
+      ),
+    );
+  }
 }
