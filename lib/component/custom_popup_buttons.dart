3f5ff95dee269e98ee444ab7ef20f4278ad6d9ac
sonw young woo <sonwoo@ecis.co.kr>
2024-11-12 11:27:07 +0900

    Resolve merge conflicts

--- lib/component/custom_popup_buttons.dart
+++ lib/component/custom_popup_buttons.dart
@@ -3,18 +3,21 @@ import 'package:flutter/material.dart';
 class CustomPopupButtons extends StatelessWidget {
 
   final String title;
+  final String name;
   final String type;
 
-  const CustomPopupButtons({super.key, required this.title, required this.type});
+  const CustomPopupButtons({super.key, required this.title, required this.type, required this.name});
 
   @override
   Widget build(BuildContext context) {
      return ElevatedButton(
        onPressed: () {
          showDialog(
+             barrierDismissible: true,
              context: context,
-           builder: (BuildContext context) {
+             builder: (BuildContext context) {
              return AlertDialog(
+              insetPadding: EdgeInsets.fromLTRB(0,80,0, 80),
               title: Text(title),
                actions: [
                  TextButton(onPressed: () {
@@ -25,7 +28,7 @@ class CustomPopupButtons extends StatelessWidget {
            }
          );
        },
-       child: null,
+       child: Text(name),
      );
   }
 
