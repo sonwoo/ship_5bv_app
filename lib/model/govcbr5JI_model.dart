63918595ffaa5ae298641304402917b909eea9a7
sonw young woo <sonwoo@ecis.co.kr>
2024-11-15 09:52:52 +0900

    Resolve merge conflicts

--- lib/model/govcbr5JI_model.dart
+++ lib/model/govcbr5JI_model.dart
@@ -2,4 +2,31 @@ import 'package:flutter/material.dart';
 
 class Govcbr5jiModel {
 
+ final String TMPSSD_KEY;
+ final String SSD_USERID;
+ final String SSD_SUN_NAME;
+ final String WORK_DIV;
+ final String SND_CHK;
+ final String TMPSSD_FD_GBN;
+ final String SSD_SIN_SANGHO;
+ final String SSD_SIN_DAY;
+ final String TMPSSD_SND_GBN;
+ final String TMPSSD_SND_GBN2;
+ final String TMPSSD_SND_GBN3;
+
+  Govcbr5jiModel({
+    required this.TMPSSD_KEY,
+    required this.SSD_USERID,
+    required this.SSD_SUN_NAME,
+    required this.WORK_DIV,
+    required this.SND_CHK,
+    required this.TMPSSD_FD_GBN,
+    required this.SSD_SIN_SANGHO,
+    required this.SSD_SIN_DAY,
+    required this.TMPSSD_SND_GBN,
+    required this.TMPSSD_SND_GBN2,
+    required this.TMPSSD_SND_GBN3,
+  });
+
+
 }
