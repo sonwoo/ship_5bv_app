2bbb41200ab9cb450e6beba52dbb2ba8cf0501c3
Yang <sksungtj@ecis.co.kr>
2024-11-15 14:21:28 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/screen/anchorage_screen.dart
+++ lib/screen/anchorage_screen.dart
@@ -31,48 +31,180 @@ class _AnchorageState extends State<Anchorage> {
     "15,000",
   ];
 
+  DateTime start_date = DateTime.now().subtract(const Duration(days: 6));
+  DateTime end_date = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('코드'),
-      ),
-      body: ListView.separated(
-        padding: EdgeInsets.all(8),
-        itemCount: comments.length,
-        itemBuilder: (context, index) {
-          return Card(
+      body: Column(
+        children: [
+          // AppBar
+          AppBar(title: Text('코드')),
+          // 상단 Container
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(width: 5.0, color: Colors.white54)),
             child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(
-                    icons[index],
-                    color: Colors.redAccent,
-                  ),
-                  title: Text('${comments[index]}'),
-                  subtitle: Text(
-                    '가격 : ${wons[index]} 원',
-                  ),
+              children: [
+                Row(
+                  children: [
+                    const Padding(padding: EdgeInsets.all(15)),
+                    const Text('신고일자'),
+                    const SizedBox(
+                      height: 30,
+                      width: 30,
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        final selectedDate = await showDatePicker(
+                          context: context,
+                          initialDate: start_date,
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime.now(),
+                          initialEntryMode: DatePickerEntryMode.calendarOnly,
+                          locale: const Locale('ko', 'KR'),
+                        );
+                        if (selectedDate != null) {
+                          setState(() {
+                            start_date = selectedDate;
+                          });
+                        }
+                      },
+                      child: Text(
+                          "${start_date.year.toString()}-${start_date.month.toString().padLeft(2, '0')}-${start_date.day.toString().padLeft(2, '0')}"),
+                    ),
+                    const Icon(Icons.accessibility_new),
+                    ElevatedButton(
+                      onPressed: () async {
+                        final selectedDate = await showDatePicker(
+                          context: context,
+                          initialDate: end_date,
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime.now(),
+                          initialEntryMode: DatePickerEntryMode.calendarOnly,
+                          locale: const Locale('ko', 'KR'),
+                        );
+                        if (selectedDate != null) {
+                          setState(() {
+                            end_date = selectedDate;
+                          });
+                        }
+                      },
+                      child: Text(
+                          "${end_date.year.toString()}-${end_date.month.toString().padLeft(2, '0')}-${end_date.day.toString().padLeft(2, '0')}"),
+                    ),
+                  ],
                 ),
-                ButtonBar(
-                  children: <Widget>[
-                    TextButton(
-                      child: Text('확인'),
-                      onPressed: () {},
-                      style: TextButton.styleFrom(
-                        textStyle: TextStyle(fontWeight: FontWeight.bold,),
-                        backgroundColor: Colors.amberAccent),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Row(
+                  children: [
+                    Padding(padding: EdgeInsets.all(15)),
+                    Text('제출번호'),
+                    SizedBox(
+                      height: 30,
+                      width: 30,
+                    ),
+                    SizedBox(
+                      width: 250, // 원하는 너비 설정
+                      height: 35,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          fillColor: Colors.white54,
+                        ),
+                      ),
                     )
                   ],
-                )
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Row(
+                  children: [
+                    Padding(padding: EdgeInsets.all(15)),
+                    Text('선박이름'),
+                    SizedBox(
+                      height: 30,
+                      width: 30,
+                    ),
+                    SizedBox(
+                      width: 250, // 원하는 너비 설정
+                      height: 35,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          fillColor: Colors.white54,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //       builder: (context) => const Govcbr5jiContents()),
+                        // );
+                      },
+                      child: const Text("   검 색   "),
+                    )),
               ],
             ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return Divider();
-        },
+          ),
+
+          Expanded(
+            child: ListView.separated(
+              padding: EdgeInsets.all(8),
+              itemCount: comments.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(
+                          icons[index],
+                          color: Colors.redAccent,
+                        ),
+                        title: Text('${comments[index]}'),
+                        subtitle: Text(
+                          '가격 : ${wons[index]} 원',
+                        ),
+                      ),
+                      ButtonBar(
+                        children: <Widget>[
+                          TextButton(
+                            child: Text('확인'),
+                            onPressed: () {},
+                            style: TextButton.styleFrom(
+                                textStyle: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                backgroundColor: Colors.amberAccent),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
