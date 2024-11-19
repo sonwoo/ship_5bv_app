ab002c21906cb012a08131081a5a44dfe146db9f
sonw young woo <sonwoo@ecis.co.kr>
2024-11-19 13:02:58 +0900

    Resolve merge conflicts

---
+++ lib/model/govcbr5JI_contents_model.dart
@@ -0,0 +1,48 @@
+class Govcbr5jiContentsModel {
+
+  final String USER_ERP_ID;
+  final String SSD_F_GBN;
+  final String SSD_MSG_GI2;
+  final String SSD_RPT_DAY;
+  final String SSD_5BV_DAY;
+  final String SSD_5BV_HM;
+  final String SSD_CRG_NM;
+  final String SSD_JUNGBAK_COD;
+  final String TMPPORT_CD;
+  final String SSD_VEHICLE_NO;
+  final String SSD_CRG_NM2;
+  final String SSD_BWH_NM;
+  final String WORK_DIV;
+  final String END_SND_CHK;
+  final String SSD_ROLE_DIV;
+  final String SSD_AGNT_CD;
+  final String SSD_AGNT_MK;
+  final String SSD_AGNT_NM;
+
+  Govcbr5jiContentsModel({required this.USER_ERP_ID, required this.SSD_F_GBN, required this.SSD_MSG_GI2, required this.SSD_RPT_DAY, required this.SSD_5BV_DAY, required this.SSD_5BV_HM, required this.SSD_CRG_NM, required this.SSD_JUNGBAK_COD, required this.TMPPORT_CD, required this.SSD_VEHICLE_NO, required this.SSD_CRG_NM2, required this.SSD_BWH_NM, required this.WORK_DIV, required this.END_SND_CHK, required this.SSD_ROLE_DIV, required this.SSD_AGNT_CD, required this.SSD_AGNT_MK, required this.SSD_AGNT_NM});
+
+  factory Govcbr5jiContentsModel.fromJson(Map<String, dynamic> json) {
+    return Govcbr5jiContentsModel(
+      USER_ERP_ID :  json['USER_ERP_ID'] ?? "",
+      SSD_F_GBN : json['SSD_F_GBN'] ?? "",
+      SSD_MSG_GI2 : json['SSD_MSG_GI2'] ?? "",
+      SSD_RPT_DAY : json['SSD_RPT_DAY'] ?? "",
+      SSD_5BV_DAY : json['SSD_5BV_DAY'] ?? "",
+      SSD_5BV_HM : json['SSD_5BV_HM'] ?? "",
+      SSD_CRG_NM : json['SSD_CRG_NM'] ?? "",
+      SSD_JUNGBAK_COD : json['SSD_JUNGBAK_COD'] ?? "",
+      TMPPORT_CD : json['TMPPORT_CD'] ?? "",
+      SSD_VEHICLE_NO : json['SSD_VEHICLE_NO'] ?? "",
+      SSD_CRG_NM2 : json['SSD_CRG_NM2'] ?? "",
+      SSD_BWH_NM : json['SSD_BWH_NM'] ?? "",
+      WORK_DIV : json['WORK_DIV'] ?? "",
+      END_SND_CHK : json['END_SND_CHK'] ?? "",
+      SSD_ROLE_DIV : json['SSD_ROLE_DIV'] ?? "",
+      SSD_AGNT_CD : json['SSD_AGNT_CD'] ?? "",
+      SSD_AGNT_MK : json['SSD_AGNT_MK'] ?? "",
+      SSD_AGNT_NM : json['SSD_AGNT_NM'] ?? "",
+    );
+  }
+
+
+}
