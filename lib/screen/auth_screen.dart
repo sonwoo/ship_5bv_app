8e6cf1a15b03ccbb992739b229bf48bdbbd6973a
Yang <sksungtj@ecis.co.kr>
2024-11-19 15:02:56 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -7,7 +7,7 @@ import 'dart:convert';
 import 'package:ship_5bv_app/util.dart';
 import 'package:ship_5bv_app/screen/home_screen.dart';
 
-enum Type {edi,cs}
+enum Type { edi, cs }
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -16,10 +16,10 @@ class AuthScreen extends StatefulWidget {
   State<AuthScreen> createState() => _AuthScreenState();
 }
 
-class _AuthScreenState extends State<AuthScreen>{
-
+class _AuthScreenState extends State<AuthScreen> {
   String _userType = 'EDI'; // 초기값으로 'EDI' 설정
-  final TextEditingController _businessNumberController = TextEditingController();
+  final TextEditingController _businessNumberController =
+      TextEditingController();
   final TextEditingController _userIdController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   bool _saveLoginInfo = false;
@@ -27,8 +27,7 @@ class _AuthScreenState extends State<AuthScreen>{
   final _targetUrl = "http://192.168.200.38:3000/api/mobileUserAuth";
 
   @override
-  void initState()
-  {
+  void initState() {
     super.initState();
     _loadSavedLoginInfo(); // 앱 실행 시 저장된 로그인 정보 불러오기
   }
@@ -47,7 +46,6 @@ class _AuthScreenState extends State<AuthScreen>{
 
   // 로그인 버튼을 클릭했을 때 실행되는 메서드
   Future<void> _login() async {
-
     String businessNumber = _businessNumberController.text.trim();
     String userId = _userIdController.text.trim();
     String password = _passwordController.text.trim();
@@ -59,7 +57,7 @@ class _AuthScreenState extends State<AuthScreen>{
       return;
     }
 
-    if(!isBusinessRegistrationNumber(businessNumber)) {
+    if (!isBusinessRegistrationNumber(businessNumber)) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('사업자번호 형식이 맞지 않습니다.')),
       );
@@ -76,10 +74,10 @@ class _AuthScreenState extends State<AuthScreen>{
     }
 
     final data = jsonEncode({
-      'USERID' : userId,
-      'PASSWORD' : password,
-      'PLATFORM' : _userType,
-      'COMPANY_NO':businessNumber
+      'USERID': userId,
+      'PASSWORD': password,
+      'PLATFORM': _userType,
+      'COMPANY_NO': businessNumber
     });
 
     Dio dio = Dio();
@@ -89,102 +87,175 @@ class _AuthScreenState extends State<AuthScreen>{
     };
 
     try {
+      var response = await dio.post(
+        _targetUrl,
+        data: data,
+      );
 
-      var response = await dio.post(_targetUrl, data: data, );
-
-      if(response.statusMessage != 'OK') {
+      if (response.statusMessage != 'OK') {
         showCustomAlertPopup(context, "알림", "서비스에 접속을 하지 못했습니다.");
-      } else
-      {
-        String ret =  response.data["Message"];
-        if(ret == "인증이 성공하였습니다."){
-            Navigator.of(context).push(MaterialPageRoute(builder: (_) => const HomeScreen()));
-          }
-          else {
+      } else {
+        String ret = response.data["Message"];
+        if (ret == "인증이 성공하였습니다.") {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (_) => const HomeScreen()));
+        } else {
           showCustomAlertPopup(context, "로그인 실패", ret);
         }
       }
-    }
-    catch(ex){
+    } catch (ex) {
       showCustomAlertPopup(context, "알림", "서비스에 접속을 하지 못했습니다.");
     }
-
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset : false,
-      appBar: AppBar(
-        title: const Text('선용품 이행완료보고'),
+      resizeToAvoidBottomInset: false,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: AppBar(
+          backgroundColor: Colors.blueAccent,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/img/ship.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+
+          title: Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              child: Text(
+                '선박용품 이행착수(완료) 보고서 App',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.red[200],
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Arial',
+                ),
+              )),
+          centerTitle: true, //title center 정렬
+          elevation: 0, //appbar 그림자 없애기
+
+          //appbar 배경 이미지 삽입
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 30,),
-            Row(
-              children: [
-                const Text('사용구분'),
-                Radio<String>(
-                  value: 'EDI',
-                  groupValue: _userType,
-                  onChanged: (value) {
-                    setState(() {
-                      _userType = value!;
-                    });
-                  },
+      body: Container(
+        color: Colors.green[50],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              // Row(
+              //   children: [
+              //     const Text('사용구분'),
+              //     Radio<String>(
+              //       value: 'EDI',
+              //       groupValue: _userType,
+              //       onChanged: (value) {
+              //         setState(() {
+              //           _userType = value!;
+              //         });
+              //       },
+              //     ),
+              //     const Text('조건부 신고'),
+              //     Radio<String>(
+              //       value: 'CS',
+              //       groupValue: _userType,
+              //       onChanged: (value) {
+              //         setState(() {
+              //           _userType = value!;
+              //         });
+              //       },
+              //     ),
+              //     const Text('EDI 선용품'),
+              //   ],
+              // ),
+              TextField(
+                controller: _businessNumberController,
+                decoration: const InputDecoration(labelText: '사업자번호'),
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly
+                ], // 숫자만 입력 가능
+              ),
+              TextField(
+                controller: _userIdController,
+                decoration: const InputDecoration(labelText: '아이디'),
+              ),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: '패스워드'),
+                obscureText: true,
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: _saveLoginInfo,
+                    onChanged: (value) {
+                      setState(() {
+                        _saveLoginInfo = value!;
+                      });
+                    },
+                  ),
+                  const Text('로그인 정보 저장'),
+                ],
+              ),
+              const SizedBox(height: 5),
+              ElevatedButton(
+                onPressed: _login,
+                child: const Text('로그인'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size.fromHeight(50), // 버튼 높이 설정
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // 모서리 둥글기를 조절
+                  ),
+                  textStyle: TextStyle(
+                    fontSize: 18,
+                    color: Colors.red[200],
+                    fontWeight: FontWeight.bold, // 글자 굵게
+                  ),
                 ),
-                const Text('조건부 신고'),
-                Radio<String>(
-                  value: 'CS',
-                  groupValue: _userType,
-                  onChanged: (value) {
-                    setState(() {
-                      _userType = value!;
-                    });
-                  },
-                ),
-                const Text('EDI 선용품'),
-              ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: '홈',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.settings),
+      //       label: '설정',
+      //     ),
+      //   ],
+      // ),
+      bottomNavigationBar: Container(
+        color: Colors.lightGreen[200],
+        height: 50,
+        child: Center(
+          child: Text(
+            '부산시 동구 중앙대로 240(초량동)현대해상빌딩 6층\nTel.051-966-7000 Fax.051-966-8000',
+            style: TextStyle(
+              fontSize: 13, // 글자 크기를 24로 설정
+              fontWeight: FontWeight.bold, // 글자 굵게
             ),
-            TextField(
-              controller: _businessNumberController,
-              decoration: const InputDecoration(labelText: '사업자번호'),
-              keyboardType: TextInputType.number,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly], // 숫자만 입력 가능
-            ),
-            TextField(
-              controller: _userIdController,
-              decoration: const InputDecoration(labelText: '아이디'),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: '패스워드'),
-              obscureText: true,
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: _saveLoginInfo,
-                  onChanged: (value) {
-                    setState(() {
-                      _saveLoginInfo = value!;
-                    });
-                  },
-                ),
-                const Text('로그인 정보 저장'),
-              ],
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _login,
-              child: const Text('로그인'),
-            ),
-          ],
+          ),
         ),
       ),
     );
@@ -199,5 +270,4 @@ class _AuthScreenState extends State<AuthScreen>{
 
     return regExp.hasMatch(value);
   }
-
 }
