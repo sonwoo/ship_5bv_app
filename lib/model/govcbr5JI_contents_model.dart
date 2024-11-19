28e229bf91ef637b10ae306bacbe234ad12ebbb7
sonw young woo <sonwoo@ecis.co.kr>
2024-11-19 16:57:35 +0900

    Resolve merge conflicts

--- lib/model/govcbr5JI_contents_model.dart
+++ lib/model/govcbr5JI_contents_model.dart
@@ -1,25 +1,26 @@
 class Govcbr5jiContentsModel {
 
-  final String USER_ERP_ID;
-  final String SSD_F_GBN;
-  final String SSD_MSG_GI2;
-  final String SSD_RPT_DAY;
-  final String SSD_5BV_DAY;
-  final String SSD_5BV_HM;
-  final String SSD_CRG_NM;
-  final String SSD_JUNGBAK_COD;
-  final String TMPPORT_CD;
-  final String SSD_VEHICLE_NO;
-  final String SSD_CRG_NM2;
-  final String SSD_BWH_NM;
-  final String WORK_DIV;
-  final String END_SND_CHK;
-  final String SSD_ROLE_DIV;
-  final String SSD_AGNT_CD;
-  final String SSD_AGNT_MK;
-  final String SSD_AGNT_NM;
+  String USER_ERP_ID;
+  String SSD_F_GBN;
+  String SSD_MSG_GI2;
+  String SSD_RPT_DAY;
+  String SSD_5BV_DAY;
+  String SSD_5BV_HM;
+  String SSD_CRG_NM;
+  String SSD_SE;
+  String SSD_JUNGBAK_COD;
+  String TMPPORT_CD;
+  String SSD_VEHICLE_NO;
+  String SSD_CRG_NM2;
+  String SSD_BWH_NM;
+  String WORK_DIV;
+  String END_SND_CHK;
+  String SSD_ROLE_DIV;
+  String SSD_AGNT_CD;
+  String SSD_AGNT_MK;
+  String SSD_AGNT_NM;
 
-  Govcbr5jiContentsModel({required this.USER_ERP_ID, required this.SSD_F_GBN, required this.SSD_MSG_GI2, required this.SSD_RPT_DAY, required this.SSD_5BV_DAY, required this.SSD_5BV_HM, required this.SSD_CRG_NM, required this.SSD_JUNGBAK_COD, required this.TMPPORT_CD, required this.SSD_VEHICLE_NO, required this.SSD_CRG_NM2, required this.SSD_BWH_NM, required this.WORK_DIV, required this.END_SND_CHK, required this.SSD_ROLE_DIV, required this.SSD_AGNT_CD, required this.SSD_AGNT_MK, required this.SSD_AGNT_NM});
+  Govcbr5jiContentsModel({required this.USER_ERP_ID, required this.SSD_F_GBN, required this.SSD_MSG_GI2, required this.SSD_RPT_DAY, required this.SSD_5BV_DAY, required this.SSD_5BV_HM, required this.SSD_CRG_NM, required this.SSD_JUNGBAK_COD, required this.TMPPORT_CD, required this.SSD_VEHICLE_NO, required this.SSD_CRG_NM2, required this.SSD_BWH_NM, required this.WORK_DIV, required this.END_SND_CHK, required this.SSD_ROLE_DIV, required this.SSD_AGNT_CD, required this.SSD_AGNT_MK, required this.SSD_AGNT_NM , required this.SSD_SE});
 
   factory Govcbr5jiContentsModel.fromJson(Map<String, dynamic> json) {
     return Govcbr5jiContentsModel(
@@ -30,6 +31,7 @@ class Govcbr5jiContentsModel {
       SSD_5BV_DAY : json['SSD_5BV_DAY'] ?? "",
       SSD_5BV_HM : json['SSD_5BV_HM'] ?? "",
       SSD_CRG_NM : json['SSD_CRG_NM'] ?? "",
+      SSD_SE : json['SSD_SE'] ?? "",
       SSD_JUNGBAK_COD : json['SSD_JUNGBAK_COD'] ?? "",
       TMPPORT_CD : json['TMPPORT_CD'] ?? "",
       SSD_VEHICLE_NO : json['SSD_VEHICLE_NO'] ?? "",
