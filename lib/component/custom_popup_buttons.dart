6385fe4fbe9df7d183753625de1b735c4b1829eb
Yang <sksungtj@ecis.co.kr>
2024-11-20 17:05:27 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/component/custom_popup_buttons.dart
+++ lib/component/custom_popup_buttons.dart
@@ -3,38 +3,47 @@ import 'package:ship_5bv_app/screen/anchorage_screen.dart';
 import 'package:ship_5bv_app/screen/stmst_screen.dart';
 
 class CustomPopupButtons extends StatelessWidget {
-
   final String title;
   final String name;
   final Widget type;
 
-  const CustomPopupButtons({super.key, required this.title, required this.type, required this.name});
+  const CustomPopupButtons(
+      {super.key, required this.title, required this.type, required this.name});
 
   @override
   Widget build(BuildContext context) {
-     return ElevatedButton(
-       onPressed: () {
-         showDialog(
-             barrierDismissible: true,
-             context: context,
-             builder: (BuildContext context) {
-             return AlertDialog(
-              insetPadding: EdgeInsets.fromLTRB(0,80,0, 80),
-              title: Text(title),
-              content: type,
-               actions: [
-                 TextButton(onPressed: () {
-                   Navigator.of(context).pop();
-                 }, child: Text("닫기"))
-               ],
-             );
-           }
-         );
-       },
-       child: Text(name),
-     );
+    return ElevatedButton(
+      onPressed: () {
+        showDialog(
+            barrierDismissible: true,
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                insetPadding: EdgeInsets.fromLTRB(0, 80, 0, 80),
+                //title: Text(title),
+                content: type,
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      style: ButtonStyle(
+                        foregroundColor:
+                            WidgetStateProperty.all(Colors.black), // 글자 색상
+                        shape: WidgetStateProperty.all(
+                          RoundedRectangleBorder(
+                            side: BorderSide(
+                                color: Colors.black, width: 1), // 테두리 설정
+                            borderRadius: BorderRadius.circular(10), // 모서리 둥글게
+                          ),
+                        ),
+                      ),
+                      child: Text("닫기"))
+                ],
+              );
+            });
+      },
+      child: Text(name),
+    );
   }
-
-
-
 }
