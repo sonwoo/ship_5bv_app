a821166b2723460299debddae68407ae2d4bfd4d
Yang <sksungtj@ecis.co.kr>
2024-11-26 16:44:54 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/component/custom_text_field.dart
+++ lib/component/custom_text_field.dart
@@ -3,39 +3,52 @@ import 'package:flutter/services.dart';
 import 'package:ship_5bv_app/const/colors.dart';
 
 class CustomTextField extends StatelessWidget {
-
   final bool isTime;
 
   final FormFieldSetter<String> onSaved;
   final FormFieldValidator<String> validator;
   final TextEditingController? controller;
   final String? initialValue;
+  final int? mLength;
 
-  const CustomTextField({super.key, required this.isTime, required this.onSaved, required this.validator, this.initialValue, this.controller});
+  const CustomTextField({
+    super.key,
+    required this.isTime,
+    required this.onSaved,
+    required this.validator,
+    this.initialValue,
+    this.controller,
+    this.mLength,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final _mLength = mLength ?? 0;
     return Expanded(
         child: TextFormField(
-              controller: controller,
-              initialValue: controller == null ? initialValue : null, // controller가 없을 때만 initialValue 사용
-              onSaved: onSaved,
-              validator: validator,
-              cursorColor: Colors.grey,
-              maxLines: 1,
-              keyboardType: isTime? TextInputType.number:null,
-              inputFormatters: isTime?
-              [
-               FilteringTextInputFormatter.digitsOnly,  // 숫자만 허용
-               LengthLimitingTextInputFormatter(4),   // 4자리로 제한
-              ]:[],
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                filled: true,
-                fillColor: TEXT_FIELD_FILL_COLOR,
-              ),
-            )
-        );
-  }
+      controller: controller,
+      initialValue: controller == null
+          ? initialValue
+          : null, // controller가 없을 때만 initialValue 사용
+      onSaved: onSaved,
+      validator: validator,
+      cursorColor: Colors.grey,
+      
+      keyboardType: isTime ? TextInputType.number : null,
+      inputFormatters: isTime
+          ? [
+              FilteringTextInputFormatter.digitsOnly, // 숫자만 허용
+              LengthLimitingTextInputFormatter(4), // 4자리로 제한
+            ]
+          : _mLength > 0
+              ? [LengthLimitingTextInputFormatter(mLength)]
+              : [],
 
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        filled: true,
+        fillColor: TEXT_FIELD_FILL_COLOR,
+      ),
+    ));
+  }
 }
